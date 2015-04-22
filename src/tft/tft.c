7a3430e3fa6ba8edacf487908b4806120a1c01d6
Andy Hughes <andyh@emerye.com>
2015-04-21 22:45:01 -0700

    Fix before fix

--- src/tft/tft.c
+++ src/tft/tft.c
@@ -24,37 +24,44 @@ void *gpio_addr = NULL;
 int mmapFD;
 
 int
-main () 
+main ()
 {
   int retval;
-  unsigned long int iCount;
+  unsigned long int iCount = 40;
   unsigned int yLine = 9;
   int xPos = 0;
   int yPos = 0;
 
- retval = MapGPIO ();
-  if (retval < 0)
+  retval = MapGPIO ();
+  if (retval < 0)
     {
-      printf ("Error attempting to map GPIO1.\n");
-    }
-   TFT_Init ();
-   TFT_Set_Address (0, 0, 239, 319);
-  TFT_Fill (GREEN);
-  TFT_Set_Address (0, 0, 239, 319);
-  TFT_Fill (RED);
-   Write_Data (BLACK);
-   TFT_Set_Address (0, 0, 239, 319);
-  Write_Data (0x0003);
-  for (xPos = 0; xPos < 240 - 32; xPos += 16)
+      printf ("Error attempting to map GPIO1.\n");
+    }
+  TFT_Init ();
+  TFT_Set_Address (0, 0, 239, 319);
+  sleep(1); 
+  TFT_Fill (GREEN);
+  sleep(1); 
+  TFT_Set_Address (0, 0, 239, 319);
+  TFT_Fill (RED);
+  sleep(1); 
+  Write_Data (BLACK);
+  TFT_Set_Address (0, 0, 239, 319);
+  Write_Data (WHITE);
+  for (xPos = 0; xPos < 240 - 32; xPos += 16)
     {
-      TFT_Char ((char) iCount, xPos, yLine, 16, BLACK, WHITE);
-      iCount++;
-    }  TFT_Fill (BLACK);
-  TFT_Text ("Temp", 0, 250, 16, BLUE, BLACK);
-  return 0;
+      TFT_Char ((char) iCount, xPos, yLine, 16, BLACK, WHITE);
+      iCount++;
+    }
+   TFT_Fill (BLACK);
+
+  TFT_Text ("This is a line.", 0, 250, 16, BLUE, BLACK);
+
+  fclose(mmapFD); 
+  return 0;
 }
 
- 
+
 /* 	
 	while(1)
 	{
@@ -139,9 +146,9 @@ main ()
 		}
 	}
 }
-*/ 
+*/
   void
-CreateButtons () 
+CreateButtons ()
 {
   WORD x1, y1, x2, y2;
    x1 = 0;
@@ -151,58 +158,61 @@ CreateButtons ()
   CreateButton (x1, y1, x2, y2, RED, GREEN, "Hour");
   CreateButton (x1 + 79, y1, x2 * 2, y2, RED, WHITE, "Min");
   CreateButton (x1 + 158, y1, x2 * 3, y2, RED, CYAN, "Test");
-}  
+} 
 
 /** 
 * Create a button. Define top left and bottom right. 
 * Border and Fill Color
 * @param x1 
-*/ 
+*/
   void
 CreateButton (WORD x1, WORD y1, WORD x2, WORD y2, WORD border, WORD backcolor,
-	      char *btnText) 
+	      char *btnText)
 {
   BYTE count = 0;
   volatile WORD textX = 0;
   volatile WORD textY = 0;
-   
+  
     //Don't let x2 > 237
     if (x2 > 237)
     x2 = 237;
-  TFT_Rectangle (x1, y1, x2, y2, border);
-  count++;
-  TFT_Rectangle (x1 + count, y1 + count, x2 + count, y2 - count, border);
-  count++;
-  TFT_Rectangle (x1 + count, y1 + count, x2 + count, y2 - count, border);
-  TFT_Box (x1 + count, y1 + count, x2 - count, y2 - count, backcolor);
-  count++;
-   textX = x1 + ((x2 - x1) / 2) - (strlen (btnText) * 8);
-  textY = y1 - 8 + (y2 - y1) / 2;
-  TFT_Text (btnText, textX + count, textY, 16, BLACK, backcolor);
-}
+  TFT_Rectangle (x1, y1, x2, y2, border);
+  count++;
+  TFT_Rectangle (x1 + count, y1 + count, x2 + count, y2 - count, border);
+  count++;
+  TFT_Rectangle (x1 + count, y1 + count, x2 + count, y2 - count, border);
+  TFT_Box (x1 + count, y1 + count, x2 - count, y2 - count, backcolor);
+  count++;
+  textX = x1 + ((x2 - x1) / 2) - (strlen (btnText) * 8);
+  textY = y1 - 8 + (y2 - y1) / 2;
+  TFT_Text (btnText, textX + count, textY, 16, BLACK, backcolor);
+}
 
 void
-WriteCommandData (unsigned int Wcommand, unsigned int Wdata) 
+WriteCommandData (unsigned int Wcommand, unsigned int Wdata)
 {
-  SendWord (Wcommand);
-  SendCommand (WRITECMD);
-  SendWord (Wdata);
-  SendCommand (WRITEDATA);
-}   void
+  SendWord (Wcommand);
+  SendCommand (WRITECMD);
+  SendWord (Wdata);
+  SendCommand (WRITEDATA);
+}
 
-TFT_Init (void) 
+void
+
+
+TFT_Init (void)
 {
-  
+
 //***********************
 //To do Reset TFT
 //      P2OUT |= TFT_RESET;
-    DelayMsec (5);
-  
+  DelayMsec (5);
+
 //      P2OUT &= ~TFT_RESET;
-    DelayMsec (15);
-  
+  DelayMsec (15);
+
 //      P2OUT |= TFT_RESET;
-    DelayMsec (15);
+  DelayMsec (15);
    WriteCommandData (0x0000, 0x0001);
   WriteCommandData (0x0003, 0xA8A4);
   WriteCommandData (0x000C, 0x0000);
@@ -247,25 +257,25 @@ TFT_Init (void)
   Write_Command (0x0022);
 }   void
 
-Write_Command (unsigned int Wcommand) 
+Write_Command (unsigned int Wcommand)
 {
   SendWord (Wcommand);
   SendCommand (WRITECMD);
 }   void
 
-Write_Data (unsigned int Wdata) 
+Write_Data (unsigned int Wdata)
 {
   SendWord (Wdata);
   SendCommand (WRITEDATA);
 }  void
 
-DelayMsec (unsigned int delay) 
+DelayMsec (unsigned int delay)
 {
   usleep (1000 * delay);
 }  void
 
 TFT_Set_Address (unsigned int PX1, unsigned int PY1, unsigned int PX2,
-		 unsigned int PY2) 
+		 unsigned int PY2)
 {
   WriteCommandData (0x0044, (PX2 << 8) + PX1);	//Horizonal RAM Address Position
   WriteCommandData (0x0045, PY1);	// Vertical RAM address start
@@ -275,16 +285,16 @@ TFT_Set_Address (unsigned int PX1, unsigned int PY1, unsigned int PX2,
   Write_Command (0x0022);
 }  void
 
-TFT_Fill (unsigned int color) 
+TFT_Fill (unsigned int color)
 {
   unsigned int i, j;
    TFT_Set_Address (0, 0, 239, 319);
   Write_Data (color);
   for (i = 0; i <= 319; i++)
-    
+
     {
       for (j = 0; j <= 239; j++)
-	
+
 	{
 	  SendCommand (WRITEDATA);
 	}
@@ -292,7 +302,7 @@ TFT_Fill (unsigned int color)
 }
 
  unsigned int
-Set_color (unsigned char r, unsigned char g, unsigned char b) 
+Set_color (unsigned char r, unsigned char g, unsigned char b)
 {
   unsigned int temp = 0;
    temp = (r & 0xF8) || (g >> 5) << 8;
@@ -302,18 +312,18 @@ Set_color (unsigned char r, unsigned char g, unsigned char b)
 }
 
  void
-ZeroMemory (char *memory, unsigned int count) 
+ZeroMemory (char *memory, unsigned int count)
 {
   unsigned char iCount;
    for (iCount = 0; iCount < count; iCount++)
-    
+
     {
       *memory++ = 0x0;
     }
 }
 
  void
-TFT_Text (char *S, WORD x, WORD y, BYTE DimFont, WORD Fcolor, WORD Bcolor) 
+TFT_Text (char *S, WORD x, WORD y, BYTE DimFont, WORD Fcolor, WORD Bcolor)
 {
   BYTE length, k;
   WORD buffer[24] =
@@ -322,18 +332,18 @@ TFT_Text (char *S, WORD x, WORD y, BYTE DimFont, WORD Fcolor, WORD Bcolor)
   BYTE charcount = 0;
    length = strlen (S);
    while (*S != 0)
-    
+
     {
       buffer[charcount] = *S;
       S++;
       charcount++;
     }
    switch (DimFont)
-    
+
     {
     case 8:
       for (k = 0; k < length; k++)
-	
+
 	{
 	  TFT_Char (buffer[k], x, y, DimFont, Fcolor, Bcolor);
 	  x = x + 8;
@@ -341,7 +351,7 @@ TFT_Text (char *S, WORD x, WORD y, BYTE DimFont, WORD Fcolor, WORD Bcolor)
       break;
      case 16:
        for (k = 0; k < length; k++)
-	
+
 	{
 	  TFT_Char (buffer[k], x, y, DimFont, Fcolor, Bcolor);
 	  x = x + 16;
@@ -350,7 +360,7 @@ TFT_Text (char *S, WORD x, WORD y, BYTE DimFont, WORD Fcolor, WORD Bcolor)
 }
 
  void
-TFT_Text32 (char *S, WORD x, WORD y, WORD Fcolor, WORD Bcolor) 
+TFT_Text32 (char *S, WORD x, WORD y, WORD Fcolor, WORD Bcolor)
 {
   BYTE length, k;
   WORD buffer[10] =
@@ -360,23 +370,23 @@ TFT_Text32 (char *S, WORD x, WORD y, WORD Fcolor, WORD Bcolor)
   BYTE iCount;
    length = strlen (S);
    while (*S != 0)
-    
+
     {
       buffer[charcount] = *S;
       S++;
       charcount++;
     }
    for (k = 0; k < length; k++)
-    
+
     {
-      
-	//Erase area in between characters
-	TFT_Set_Address ((x + 32), y, (x + 35), y + 32);
+
+      //Erase area in between characters
+      TFT_Set_Address ((x + 32), y, (x + 35), y + 32);
       Write_Data (Bcolor);
       for (iCount = 0; iCount < 4 * 32; iCount++)
-	
+
 	{
-	  
+
 //                              P2OUT |= TFT_WR;
 //                              P2OUT &= ~TFT_WR;
 //                              P2OUT |= TFT_WR;
@@ -388,7 +398,7 @@ TFT_Text32 (char *S, WORD x, WORD y, WORD Fcolor, WORD Bcolor)
 
   void
 TFT_32Char (char C1, unsigned int x, unsigned int y, unsigned int Fcolor,
-	    unsigned int Bcolor) 
+	    unsigned int Bcolor)
 {
   unsigned char *ptrFont;
   unsigned int Cptrfont;
@@ -399,21 +409,21 @@ TFT_32Char (char C1, unsigned int x, unsigned int y, unsigned int Fcolor,
   ptrFont = ptrFont + Cptrfont;
    Set_GDRAM_Address (x, y);
   for (i = 0; i < 32; i++)
-    
+
     {
       Set_GDRAM_Address (x, y + i);
       for (k = 0; k < 4; k++)
-	
+
 	{
 	  for (lineCount = 0; lineCount < 8; lineCount++)
-	    
+
 	    {
 	      cbit = (*ptrFont << lineCount) & 0x80;
 	      if (cbit == 0x80)
 		Write_Data (Fcolor);
-	      
+
 	      else
-		
+
 		{
 		  Write_Data (Bcolor);
 		}
@@ -425,7 +435,7 @@ TFT_32Char (char C1, unsigned int x, unsigned int y, unsigned int Fcolor,
 
   void
 TFT_Char (char C1, unsigned int x, unsigned int y, unsigned char DimFont,
-	  unsigned int Fcolor, unsigned int Bcolor) 
+	  unsigned int Fcolor, unsigned int Bcolor)
 {
   unsigned char *ptrFont;
   unsigned int *ptrintFont;
@@ -435,10 +445,10 @@ TFT_Char (char C1, unsigned int x, unsigned int y, unsigned char DimFont,
   unsigned int k, i, x_new, print1, print2;
   unsigned int print3;
    switch (DimFont)
-    
+
     {
      case 8:
-      
+
       {
 	ptrFont = (unsigned char *) FONT_8x8;
 	Cptrfont = (C1 - 32) * 8;
@@ -447,28 +457,28 @@ TFT_Char (char C1, unsigned int x, unsigned int y, unsigned char DimFont,
 	x_new = x;
 	y_new = y;
 	 for (k = 0; k < 8; k++)
-	  
+
 	  {
 	    font8x8[k] = *ptrFont;
 	    ptrFont++;
 	  }
 	 for (i = 0; i < 8; i++)
-	  
+
 	  {
 	    for (k = 0; k < 8; k++)
-	      
+
 	      {
 		print1 = (font8x8[i] & 0x80);
 		print2 = print1 >> 7;
 		if (print2 == 1)
-		  
+
 		  {
 		    TFT_Dot (x_new, y_new, Fcolor);
 		    x_new++;
 		  }
-		
+
 		else
-		  
+
 		  {
 		    TFT_Dot (x_new, y_new, Bcolor);
 		    x_new++;
@@ -483,40 +493,40 @@ TFT_Char (char C1, unsigned int x, unsigned int y, unsigned char DimFont,
       }
       break;
      case 16:
-      
+
       {
 	ptrintFont = (unsigned int *) FONT_16x16;
 	Cptrfont = (C1 - 32) * 16;
 	ptrintFont = ptrintFont + Cptrfont;
 	 TFT_Set_Address (x, y, x + 15, y + 15);
 	 for (k = 0; k < 16; k++)
-	  
+
 	  {
 	    font16x16[k] = *ptrintFont;
 	    ptrintFont++;
 	  }
 	 for (i = 0; i < 16; i++)
-	  
+
 	  {
 	    for (k = 0; k < 16; k++)
-	      
+
 	      {
 		print3 = (font16x16[i] & 0x8000);
 		if ((print3 >> 15) == 1)
-		  
+
 		  {
-		    
-		      //TFT_Dot(x_new,y_new,Fcolor);
-		      Write_Data (Fcolor);
+
+		    //TFT_Dot(x_new,y_new,Fcolor);
+		    Write_Data (Fcolor);
 		    x_new++;
 		  }
-		
+
 		else
-		  
+
 		  {
-		    
-		      //TFT_Dot(x_new,y_new,Bcolor);
-		      Write_Data (Bcolor);
+
+		    //TFT_Dot(x_new,y_new,Bcolor);
+		    Write_Data (Bcolor);
 		    x_new++;
 		  }
 		font16x16[i] = font16x16[i] << 1;
@@ -528,34 +538,31 @@ TFT_Char (char C1, unsigned int x, unsigned int y, unsigned char DimFont,
 
   void
 TFT_Box (unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,
-	 unsigned int color) 
+	 unsigned int color)
 {
    unsigned int i, j;
    TFT_Set_Address (x1, y1, x2, y2);
   Write_Data (color);
   for (i = y1; i <= y2; i++)
-    
+
     {
       for (j = x1; j <= x2; j++)
-	
+
 	{
-	  
-//                      P2OUT |= TFT_WR;
-//                      P2OUT &= ~TFT_WR;
-//                      P2OUT |= TFT_WR;
+        SendCommand(WRITEDATA); 
 	}
     }
 }
 
  void
-TFT_Dot (unsigned int x, unsigned int y, unsigned int color) 
+TFT_Dot (unsigned int x, unsigned int y, unsigned int color)
 {
   TFT_Set_Address (x, y, x, y);
   Write_Data (color);
 }   void
 
 TFT_Line (unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,
-	  unsigned int color) 
+	  unsigned int color)
 {
   int x, y, addx, addy, dx, dy;
   long int P;
@@ -565,43 +572,43 @@ TFT_Line (unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,
   x = x1;
   y = y1;
    if (x1 > x2)
-    
+
     {
       addx = -1;
     }
-  
+
   else
-    
+
     {
       addx = 1;
        if (y1 > y2)
-	
+
 	{
 	  addy = -1;
 	}
-      
+
       else
-	
+
 	{
 	  addy = 1;
 	}
         if (dx >= dy)
-	
+
 	{
 	  P = (2 * dy) - dx;
 	  for (i = 1; i < dx + 2; i++)
-	    
+
 	    {
 	      TFT_Dot (x, y, color);
 	      if (P < 0)
-		
+
 		{
 		  P = P + (2 * dy);
 		  x = (x + addx);
 		}
-	      
+
 	      else
-		
+
 		{
 		  P = P + (2 * dy) - (2 * dx);
 		  x = x + addx;
@@ -609,23 +616,23 @@ TFT_Line (unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,
 		}
 	    }
 	}
-      
+
       else
-	
+
 	{
 	  P = (2 * dx) - dy;
 	  for (i = 1; i < dy + 2; i++)
 	    TFT_Dot (x, y, color);
 	}
       if (P < 0)
-	
+
 	{
 	  P = P + (2 * dx);
 	  y = y + addy;
 	}
-      
+
       else
-	
+
 	{
 	  P = P + (2 * dx) - (2 * dy);
 	  x = x + addx;
@@ -636,11 +643,11 @@ TFT_Line (unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2,
 
  void
 TFT_H_Line (unsigned char x1, unsigned char x2, unsigned char y_pos,
-	    unsigned int color) 
+	    unsigned int color)
 {
   unsigned int k;
    for (k = x1; k <= x2; k++)
-    
+
     {
       TFT_Dot (k, y_pos, color);
     }
@@ -648,21 +655,21 @@ TFT_H_Line (unsigned char x1, unsigned char x2, unsigned char y_pos,
 
   void
 TFT_V_Line (unsigned int y1, unsigned int y2, unsigned char x_pos,
-	    unsigned int color) 
+	    unsigned int color)
 {
   unsigned int k;
   for (k = y1; k <= y2; k++)
-    
+
     {
       TFT_Dot (x_pos, k, color);
     }
 }
 
- 
-/* Writes an unfilled rectangle */ 
+
+/* Writes an unfilled rectangle */
   void
 TFT_Rectangle (unsigned int x1, unsigned int y1, unsigned int x2,
-	       unsigned int y2, unsigned int color) 
+	       unsigned int y2, unsigned int color)
 {
   TFT_H_Line (x1, x2, y1, color);
   TFT_H_Line (x1, x2, y2, color);
@@ -670,26 +677,26 @@ TFT_Rectangle (unsigned int x1, unsigned int y1, unsigned int x2,
   TFT_V_Line (y1, y2, x2, color);
 }   void
 
-TFT_Circle (WORD x, WORD y, BYTE radius, BYTE fill, WORD color) 
+TFT_Circle (WORD x, WORD y, BYTE radius, BYTE fill, WORD color)
 {
   int a_, b_, P;
   a_ = 0;
   b_ = radius;
   P = 1 - radius;
   while (a_ <= b_)
-    
+
     {
       if (fill == 1)
-	
+
 	{
 	  TFT_Line (x - a_, y + b_, x + a_, y + b_, color);
 	  TFT_Line (x - a_, y - b_, x + a_, y - b_, color);
 	  TFT_Line (x - b_, y + a_, x + b_, y + a_, color);
 	  TFT_Line (x - b_, y - a_, x + b_, y - a_, color);
 	}
-      
+
       else
-	
+
 	{
 	  TFT_Dot (a_ + x, b_ + y, color);
 	  TFT_Dot (b_ + x, a_ + y, color);
@@ -701,14 +708,14 @@ TFT_Circle (WORD x, WORD y, BYTE radius, BYTE fill, WORD color)
 	  TFT_Dot (x - b_, y - a_, color);
 	}
        if (P < 0)
-	
+
 	{
 	  P = (P + 3) + (2 * a_);
 	  a_ += 1;
 	}
-      
+
       else
-	
+
 	{
 	  P = (P + 5) + (2 * (a_ - b_));
 	  a_ += 1;
@@ -717,25 +724,25 @@ TFT_Circle (WORD x, WORD y, BYTE radius, BYTE fill, WORD color)
     }
 }
 
-    
+   
 //Set Absolute GDRAM address. x Range is 0-319. Y Range is 0-239; 
 //This sets the absolute address for writes. 
   void
-Set_GDRAM_Address (int x1, int y1) 
+Set_GDRAM_Address (int x1, int y1)
 {
   WriteCommandData (0x004E, x1);	// Set GDDRAM X address
   WriteCommandData (0x004F, y1);	// Set GDDRAM Y address
   Write_Command (0x22);
 }  void
 
-TestLargeFont (void) 
+TestLargeFont (void)
 {
   int yStart, iCount;
   char mydigit = '0';
    TFT_Fill (BLACK);
   yStart = 230;
   for (iCount = 0; iCount < 2; iCount++)
-    
+
     {
       TFT_32Char (mydigit, 0, yStart, WHITE, BLUE);
       mydigit = mydigit + 1;
@@ -752,7 +759,7 @@ TestLargeFont (void)
   TFT_Text32 ("012345", 0, 230, GREEN, RED);
 }
 
-   
+  
 /*
 sub procedure TFT_Image(dim pos_x,pos_y,dim_x,dim_y as word,dim const picture as ^byte)
 dim k,i,x,y,temp as word
@@ -785,4 +792,4 @@ inc(y)
 next k
 TFT_CS  = 1
 end sub
-*/ 
+*/
