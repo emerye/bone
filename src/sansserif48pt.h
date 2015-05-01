f3f19effbda5d56137a12fb19eca04e0ecf5eee7
Andy Hughes <andyh@emerye.com>
2015-04-30 19:48:08 -0700

    SansSerif 48pt font

---
+++ src/sansserif48pt.h
@@ -0,0 +1,4 @@
+// Font data for Microsoft Sans Serif 48pt
+extern const uint_8 microsoftSansSerif48ptBitmaps[];
+extern const FONT_INFO microsoftSansSerif48ptFontInfo;
+extern const FONT_CHAR_INFO microsoftSansSerif48ptDescriptors[];
