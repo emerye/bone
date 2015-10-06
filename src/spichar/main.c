6af90fb588eb2bc4706650948140f56e7c5e0ff4
Andy Hughes <andyh@emerye.com>
2015-10-05 21:56:08 -0700

    Before refactoring

--- src/spichar/main.c
+++ src/spichar/main.c
@@ -7,6 +7,9 @@
 #include <stdint.h>
 #include <linux/spi/spidev.h>
 #include <time.h>
+#include <prussdrv.h>
+#include <pruss_intc_mapping.h>
+#include <pthread.h>
 #include "spictl.h"
 #include "lcdchar.h"
 #include "tmp100.h"
@@ -14,81 +17,83 @@
 #define TMP100ADDR 0x48
 #define SPI_PATH "/dev/spidev1.0"
 
-char *gpioExport = "/sys/class/gpio/export"; 
-int ledState; 
+char *gpioExport = "/sys/class/gpio/export";
+int ledState;
 
-void SetupGPIO() 
-{
-  system("echo 49 > /sys/class/gpio/export"); 
-}
 
-void LEDState() 
+void
+SetupGPIO ()
 {
-if (ledState ==  1) 
-{
-  ledState = 0; 
-  system("echo 1 > /sys/class/gpio/gpio49/value"); 
-}
-else 
-{
-  system("echo 0 > /sys/class/gpio/gpio49/value"); 
-  ledState = 1; 
+  system ("echo 49 > /sys/class/gpio/export");
 }
 
-  int gpioStatus = 0; 
+void
+LEDState ()
+{
+  if (ledState == 1)
+    {
+      ledState = 0;
+      system ("echo 1 > /sys/class/gpio/gpio49/value");
+    }
+  else
+    {
+      system ("echo 0 > /sys/class/gpio/gpio49/value");
+      ledState = 1;
+    }
 }
-  
 
 
 int
 main ()
 {
-  time_t rawTime; 
-  int iretVal,i;
-  unsigned char cReceiveData[50];
-  char tempReading[50]; 
-  char date[50]; 
-  struct tm *timeinfo;  
+  time_t rawTime;
+  int iretVal, i;
+  char tempReading[50];
+  char date[50];
+  struct tm *timeinfo;
 
-  iretVal = initSPI();
-  if (iretVal) {
-    printf("Not able to open SPI device. Program will exit.\n");
-    return -1;
-   }
+  iretVal = initSPI ();
+  if (iretVal)
+    {
+      printf ("Not able to open SPI device. Program will exit.\n");
+      return -1;
+    }
 
-  iretVal = initTMP100(TMP100ADDR);
-  if (iretVal) {
-    printf("Not able to open I2C device. Program will exit.\n");
-    return -1;
-   }
- memset (cReceiveData, 0, 50);
+  iretVal = initTMP100 (TMP100ADDR);
+  if (iretVal)
+    {
+      printf ("Not able to open I2C device. Program will exit.\n");
+      return -1;
+    }
 
- Setup4bit();
- SetupGPIO(); 
+  Setup4bit ();
+  SetupGPIO ();
 
- for (i=0; i<1000000; i++) 
-{
- time(&rawTime); 
- timeinfo =localtime(&rawTime); 
- strftime(date, 50, "%c", timeinfo); 
- strftime(date, 50, "%r", timeinfo); 
- WriteString(0,0, date);  
+  for (i = 0; i < 1000000; i++)
+    {
+      time (&rawTime);
+      timeinfo = localtime (&rawTime);
+      strftime (date, 50, "%c", timeinfo);
+      strftime (date, 50, "%r", timeinfo);
+      WriteString (0, 0, date);
 
- printf("Time: %s\n",date);  
+      printf ("Time: %s\n", date);
 
- iretVal = ReadTemperature(tempReading); 
- if(iretVal < 0) {
-   puts("Failed to read temperature.\n"); 
-  } else {
-  printf("Temperature reading is %s\n",tempReading); 
-  WriteString(3,0,"             "); 
-  WriteString(3,0,tempReading); 
-  }
- sleep(1); 
- LEDState(); 
-}
-  close (i2cfd);    //Close i2c
-  close (SPIfd);    //close spi 
+      iretVal = ReadTemperature (tempReading);
+      if (iretVal < 0)
+	{
+	  puts ("Failed to read temperature.\n");
+	}
+      else
+	{
+	  printf ("Temperature reading is %s\n", tempReading);
+	  WriteString (3, 0, "             ");
+	  WriteString (3, 0, tempReading);
+	}
+      sleep (1);
+      LEDState ();
+    }
+  close (i2cfd);		//Close i2c
+  close (SPIfd);		//close spi 
   return 0;
 }
-
