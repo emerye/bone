1cc1fde53d77be7184e0dfc3faff867a8fc2424c
Andy Hughes <andyh@emerye.com>
2015-10-16 19:38:43 -0700

    Updated

--- src/i2clcd/i2clcd.c
+++ src/i2clcd/i2clcd.c
@@ -3,34 +3,35 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
+#include "lcdchar.h"
 
 //install i2c-tools, libi2c-dev
 int i2caddr = 0x27; 
 unsigned char i2creg = 0; 
-
+int i2cfd; 
 
 int
 main (int argc, char **argv)
 {
   int r, i;
-  int fd;
+  int i2cfd;
 
   char *dev = "/dev/i2c-1";
 
-  fd = open (dev, O_RDWR);
-  if (fd < 0)
+  i2cfd = open (dev, O_RDWR);
+  if (i2cfd < 0)
     {
       perror ("Opening i2c device node.\n");
       return -1;
     }
 
-  r = ioctl (fd, I2C_SLAVE, i2caddr);
+  r = ioctl (i2cfd, I2C_SLAVE, i2caddr);
   if (r < 0)
     perror ("Selecting i2c device.\n");
 
   for (i=0; i < 100; i++) {
   i2creg |= 0x08; 
-  r = i2c_smbus_write_byte(fd, i2creg);
+  r = i2c_smbus_write_byte(i2cfd, i2creg);
   if (r < 0)
     {
       perror ("Error writing to config register.\n");
@@ -38,7 +39,7 @@ main (int argc, char **argv)
   usleep(100000); 
     
   i2creg &= ~0x08; 
-  r = i2c_smbus_write_byte(fd, i2creg);
+  r = i2c_smbus_write_byte(i2cfd, i2creg);
   if (r < 0)
     {
       perror ("Error writing to config register.\n");
@@ -46,6 +47,6 @@ main (int argc, char **argv)
   usleep(100000); 
  }
 
-  close (fd);
+  close (i2cfd);
   return 0;
 }
