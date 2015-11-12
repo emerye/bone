9392ee86fa184cf67713b5efbd78fa5f3e2cc52f
Andy Hughes <andyh@emerye.com>
2015-11-11 21:16:41 -0800

    Add comments

--- src/i2clcd/lcdchar.h
+++ src/i2clcd/lcdchar.h
@@ -15,10 +15,37 @@
 #define BIT6 0x40 
 #define BIT7 0x80 
 
-
+/**
+ * Write a byte to the display. 
+ *
+ * @param i2cfd open i2c driver reference
+ * @param bytetoWrite Byte to write
+ * @param cmd 0=Command 1=Data
+ */ 
 extern void WriteI2CByte(int i2cfd, unsigned char bytetoWrite, int cmd); 
+
+/**
+ * Set up 4 bit interface
+ *
+ * @param i2cfd File descriptor 
+ */
 extern void Setup4bit(int i2cfd); 
+
+/**
+ * Clear Display
+ *
+ * @param i2cfd File descriptor 
+ */
 extern void DisplayClear(int i2cfd); 
+
+/**
+ * Write a string to the display at starting position
+ *
+ * @param i2cfd File descriptor 
+ * @param row Starting row number
+ * @param ypos Starting y position
+ * @param message[] String to send. 
+ */
 extern void WriteString(int i2cfd, int row, int ypos, char message[]); 
 
 #endif /* LCDCHAR_H_ */
