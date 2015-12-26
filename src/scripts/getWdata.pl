2d6ac53959802a1eb9472bc6a7fe83a53736259e
Andy Hughes <andyh@emerye.com>
2015-12-26 09:21:32 -0800

    Update with ID

--- src/scripts/getWdata.pl
+++ src/scripts/getWdata.pl
@@ -38,7 +38,7 @@ $OUTFILE = '/root/bone/src/scripts/cweather.json';
 
 #$wgetcmd = "wget -O " . $OUTFILE . ' http://api.openweathermap.org/data/2.5/weather?q=Fremont'; 
 #
-$wgetcmd = 'curl -o fremont.json "http://api.openweathermap.org/data/2.5/weather?id=5350734&appid=86fd40302a1f892f8cd7528036289523';
+$wgetcmd = 'curl -o /root/bone/src/scripts/cweather.json "http://api.openweathermap.org/data/2.5/weather?id=5350734&appid=86fd40302a1f892f8cd7528036289523"';
 
 
 $ret = system($wgetcmd); 
