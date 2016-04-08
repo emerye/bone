292fef4d83f359e5be19055d9c48ce646bcbb4de
Andy Hughes <andyh@emerye.com>
2016-04-08 16:55:33 -0700

    Update

--- msp43xwrk/MI2CMaster/main.c
+++ msp43xwrk/MI2CMaster/main.c
@@ -26,6 +26,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "lcdchar.h"
+#include "nmea/nmea.h"
 
 unsigned char TXData;
 unsigned char TXByteCtr;
@@ -34,8 +35,25 @@ unsigned int elapsedSeconds = 0;
 unsigned long lifeTimeMinutes;
 unsigned char updateDisplay = 0;
 unsigned char serialBufferDisplayFlag = 0;
-char serialBuffer[256];
+char serialBuffer[200];
 char *ptrSerialBuffer;
+
+nmeaINFO info;
+nmeaPARSER parser;
+
+
+const char *bufferlines[] = {
+      "$GPRMC,173843,A,3349.896,N,11808.521,W,000.0,360.0,230108,013.4,E*69\r\n",
+      "$GPGGA,111609.14,5001.27,N,3613.06,E,3,08,0.0,10.2,M,0.0,M,0.0,0000*70\r\n",
+      "$GPGSV,2,1,08,01,05,005,80,02,05,050,80,03,05,095,80,04,05,140,80*7f\r\n",
+      "$GPGSV,2,2,08,05,05,185,80,06,05,230,80,07,05,275,80,08,05,320,80*71\r\n",
+      "$GPGSA,A,3,01,02,03,04,05,06,07,08,00,00,00,00,0.0,0.0,0.0*3a\r\n",
+      "$GPRMC,111609.14,A,5001.27,N,3613.06,E,11.2,0.0,261206,0.0,E*50\r\n",
+      "$GPVTG,217.5,T,208.8,M,000.00,N,000.01,K*4C\r\n"
+  };
+
+  int it;
+
 
 /**
  * Delay 1 msec with a 4MHz clock
@@ -231,6 +249,24 @@ unsigned long ReadFlashWord() {
 	return *FlashPtr;
 }
 
+int parse1()
+{
+	int bufsize;
+
+    nmea_zero_INFO(&info);
+    nmea_parser_init(&parser);
+
+    const char * buffer = "$GPRMC,173843,A,3349.896,N,11808.521,W,000.0,360.0,230108,013.4,E*69\r\n";
+    bufsize = strlen(buffer);
+    printf("Size %d\n", bufsize);
+
+    nmea_parse(&parser, buffer, (int)strlen(buffer), &info);
+
+    nmea_parser_destroy(&parser);
+
+    return 0;
+}
+
 
 int main(void) {
 
@@ -262,9 +298,12 @@ int main(void) {
 
 	lifeTimeMinutes = ReadFlashWord(); 			//Read lifetime minutes from flash
 
+
 	WriteLCDByte('A', 1);
 	DelayMsec(3);
 	WriteLCDByte('B', 1);
+
+	//parse1();
 
 	sprintf(timeBuffer, "%3d minutes elapsed", 0);
 	sprintf(lifeBuffer, "%3lu hours lifetime", lifeTimeMinutes/60);
