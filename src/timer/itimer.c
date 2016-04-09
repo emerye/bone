6f35546ce5daa581bd7ff088a130d9dc7678e17d
Andy Hughes <emerye@yahoo.com>
2016-04-09 03:22:25 -0700

    add Cumulative

--- src/timer/itimer.c
+++ src/timer/itimer.c
@@ -2,33 +2,61 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-void timer_handler (int signum)
+
+//Interval Timer Handler
+void timer_handler(int signum)
 {
- static int count = 0;
- printf ("timer expired %d times\n", ++count);
+    static int count = 0;
+    int cumcount = 0;
+    unsigned char buf[100];
+    FILE *fp;
+
+    if ((fp = fopen("./cumcount", "r+")) == NULL) {
+	puts("Error opening file\n");
+	return;
+    }
+
+    int retval = fread((void *) &cumcount, 4, 1, fp);
+    if (retval < 0) {
+	printf("read error %d\n", errno);
+    }
+    cumcount++;
+    fseek(fp, 0, SEEK_SET);
+
+    fwrite(&cumcount, 4, 1, fp);
+
+    printf("Elasped: %d Cumcount: %d\n", ++count, cumcount);
+
+    fclose(fp);
 }
 
-int main ()
+
+int main()
 {
- struct sigaction sa;
- struct itimerval timer;
+    struct sigaction sa;
+    struct itimerval timer;
 
- /* Install timer_handler as the signal handler for SIGVTALRM. */
- memset (&sa, 0, sizeof (sa));
- sa.sa_handler = &timer_handler;
- sigaction (SIGVTALRM, &sa, NULL);
+    /* Install timer_handler as the signal handler for SIGVTALRM. */
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = &timer_handler;
+    sigaction(SIGVTALRM, &sa, NULL);
 
- /* Configure the timer to expire after 250 msec... */
- timer.it_value.tv_sec = 0;
- timer.it_value.tv_usec = 250000;
- /* ... and every 250 msec after that. */
- timer.it_interval.tv_sec = 0;
- timer.it_interval.tv_usec = 250000;
- /* Start a virtual timer. It counts down whenever this process is
-   executing. */
- setitimer (ITIMER_VIRTUAL, &timer, NULL);
+    /* Configure the timer to expire after 250 msec... */
+    timer.it_value.tv_sec = 1;
+    timer.it_value.tv_usec = 0;
+    /* ... and every 250 msec after that. */
+    timer.it_interval.tv_sec = 1;
+    timer.it_interval.tv_usec = 0;
+    /* Start a virtual timer. It counts down whenever this process is
+       executing. */
+    setitimer(ITIMER_VIRTUAL, &timer, NULL);
 
- /* Do busy work. */
- while (1);
+    /* Do busy work. */
+    while(1); 
 }
