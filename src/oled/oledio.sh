e274eb02f08c3c850b4aed1d67f10278b2b2ecc2
Andy Hughes <andyh@emerye.com>
2016-05-29 21:58:48 -0700

    Add explaination

--- src/oled/oledio.sh
+++ src/oled/oledio.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo spi > /sys/devices/platform/ocp/ocp:P9_18_pinmux/state
+# echo spi > /sys/devices/platform/ocp/ocp:P9_18_pinmux/state
 config-pin P9_17 spi
 config-pin P9_18 spi
 config-pin P9_21 spi
 config-pin P9_22 spi
 config-pin P9_14 gpio
 config-pin P9_16 gpio
+echo "Pin Configuration"
 config-pin -q P9_17
 config-pin -q P9_18
 config-pin -q P9_21
