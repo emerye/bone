4e7b13f155e27b86710df612638892878074bcb8
Andy Hughes <andyh@emerye.com>
2016-10-09 09:56:03 -0700

    Update with latest

--- src/i2clcd/lcdlibtest.c
+++ src/i2clcd/lcdlibtest.c
@@ -11,80 +11,77 @@
 int i2caddr = 0x27;
 unsigned char i2creg = 0;
 int i2cfd;
-char timeBuff[50]; 
-char dateBuff[50]; 
+char timeBuff[50];
+char dateBuff[50];
 
-char * GetTime() 
+char *GetTime()
 {
-  time_t curTime; 
-  struct tm * timeInfo;
+    time_t curTime;
+    struct tm *timeInfo;
 
-  time(&curTime); 
-  timeInfo = localtime(&curTime);  
-  strftime(timeBuff, 50, "%r", timeInfo);  
-  strftime(dateBuff, 50, "%D", timeInfo); 
-  return timeBuff; 
+    time(&curTime);
+    timeInfo = localtime(&curTime);
+    strftime(timeBuff, 50, "%r", timeInfo);
+    strftime(dateBuff, 50, "%D", timeInfo);
+    return timeBuff;
 }
 
 
-void Display(int i2cfd, unsigned char tgtAddress) 
+void Display(int i2cfd, unsigned char tgtAddress)
 {
-  int r; 
+    int r;
 
-  r = ioctl (i2cfd, I2C_SLAVE, tgtAddress);
-  if (r < 0) {
-    perror ("Selecting i2c device.\n");
-  }
-  GetTime(); 
-  WriteString (i2cfd, 0,0, dateBuff); 
-  WriteString (i2cfd, 1,0, GetTime()); 
-}
-
-int
-main (int argc, char **argv)
-{
-  int r;
-  int i2cfd; 
-  char *dev = "/dev/i2c-1";
-
-  i2cfd = open (dev, O_RDWR);
-  if (i2cfd < 0)
-    {
-      perror ("Opening i2c device node.\n");
-      return -1;
+    r = ioctl(i2cfd, I2C_SLAVE, tgtAddress);
+    if (r < 0) {
+	perror("Selecting i2c device.\n");
     }
-  r = ioctl (i2cfd, I2C_SLAVE, i2caddr);
-  if (r < 0)
-    perror ("Selecting i2c device.\n");
+    GetTime();
+    WriteString(i2cfd, 0, 0, dateBuff);
+    WriteString(i2cfd, 1, 0, GetTime());
+}
 
-  Setup4bit(i2cfd);  
-  DisplayClear(i2cfd);
-  Display(i2cfd, i2caddr);
+int main(int argc, char **argv)
+{
+    int r;
+    int i2cfd;
+    char *dev = "/dev/i2c-1";
 
-  r = ioctl (i2cfd, I2C_SLAVE, i2caddr);
-  if (r < 0)
-    perror ("Selecting i2c device.\n");
-  Setup4bit(i2cfd);
+    i2cfd = open(dev, O_RDWR);
+    if (i2cfd < 0) {
+	perror("Opening i2c device node.\n");
+	return -1;
+    }
+    r = ioctl(i2cfd, I2C_SLAVE, i2caddr);
+    if (r < 0)
+	perror("Selecting i2c device.\n");
+
+    Setup4bit(i2cfd);
+    DisplayClear(i2cfd);
+    Display(i2cfd, i2caddr);
+
+    r = ioctl(i2cfd, I2C_SLAVE, i2caddr);
+    if (r < 0)
+	perror("Selecting i2c device.\n");
+    Setup4bit(i2cfd);
 
 //  WriteI2CByte(0x0F, 0); 
 
-  DisplayClear (i2cfd);
-  GetTime(); 
-  WriteString (i2cfd, 0,0, dateBuff); 
-  WriteString (i2cfd, 1,0, GetTime()); 
+    DisplayClear(i2cfd);
+    GetTime();
+    WriteString(i2cfd, 0, 0, dateBuff);
+    WriteString(i2cfd, 1, 0, GetTime());
 
 //  DisplayClear ();
-//  WriteString (3, 0, "Hello There");
+    WriteString(i2cfd, 2, 0, "Going to");
+    WriteString(i2cfd, 3, 0, "Thunder Valley");
 
-  while(1) 
-  {
-  	GetTime(); 
-  WriteString (i2cfd,0,0, dateBuff); 
-  WriteString (i2cfd, 1,0, GetTime()); 
-  
-  sleep(1); 
-}
+    while (1) {
+	GetTime();
+	WriteString(i2cfd, 0, 0, dateBuff);
+	WriteString(i2cfd, 1, 0, GetTime());
+	sleep(1);
+    }
 
-  close (i2cfd);
-  return 0;
+    close(i2cfd);
+    return 0;
 }
