3f9cc9161a79a8b3e34bdb7918bd5bdc4ba62c1f
Andy Hughes <andyh@emerye.com>
2016-10-31 07:16:11 -0700

    Add I2C Master

--- winscripts/rbackup.cmd
+++ winscripts/rbackup.cmd
@@ -1,1 +1,4 @@
+
+SET PROGRAMFILES=%PROGRAMFILES(x86)%
+
 c:\cygwin\bin\bash -c /cygdrive/c/users/andy/Documents/scripts/system/rbackup.sh
--- winscripts/rbackup.sh
+++ winscripts/rbackup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # shell script for daily backup  
-/usr/bin/rsync -av --delete /cygdrive/c/users/andy/Documents /cygdrive/c/business /cygdrive/c/emery andy@rbackup:/backup 
+
+/usr/bin/rsync -av --delete /cygdrive/c/users/andy/Documents /cygdrive/c/business /cygdrive/c/emery 'andy@rbackup:/backup' 
