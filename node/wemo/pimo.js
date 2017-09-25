c440efa61a9805b3aaa33ec8325770e923064e1b
Andy Hughes <andyh@emerye.com>
2017-09-24 18:47:40 -0700

    Add device

--- node/wemo/pimo.js
+++ node/wemo/pimo.js
@@ -23,10 +23,14 @@ let fauxMo = new FauxMo({
             /*      handler: (action) => {
                 console.log('office fan action:', action);
 		
-            }
-      */
+            } */ 
             handler: (action) => write(action)
-        }
+        }, 
+        {
+            name: 'Wandas light',
+            port: 11002,
+            handler: (action) => write(action)
+        }, 
     ]
 });
 
