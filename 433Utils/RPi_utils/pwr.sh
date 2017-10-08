2891bef06294eb8f1ee67bb7a42d8933ee83dbf8
Andy Hughes <andyh@emerye.com>
2017-10-07 21:32:11 -0700

    Add script

---
+++ 433Utils/RPi_utils/pwr.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+device=$1
+operation=$2
+
+case "$device" in 
+"1") 
+  if [ $operation = "on" ]; then 
+    ./codesend 1381683 1 150
+    echo "Channel one on" 
+  else 
+    ./codesend 1381692 1 150
+    echo "Channel one off" 
+  fi
+;;
+"2") 
+  if [ $operation = "on" ]; then 
+    ./codesend 1381683 1 150
+    echo "Channel two on" 
+  else 
+    ./codesend 1381692 1 150
+    echo "Channel two off" 
+  fi
+;;
+"3") 
+  if [ $operation = "on" ]; then 
+    ./codesend 1382147 1 150
+    echo "Channel three  on" 
+  else 
+    ./codesend 1382156 1 150
+    echo "Channel three off" 
+  fi
+;;
+esac    
+
+#Channel 
+#./codesend 1381683 1 150
+
+#Channel 1 off
+#./codesend 1381692 1 150
+
+
+#Channel 3 On
+#./codesend 1382147 1 150 
+#Channel 3 Off
+#./codesend 1382156 1 150 
+
+
