425cb27b3865ade36fd43d2d54533fa71b5b9e3e
Andy Hughes <andyh@emerye.com>
2017-10-08 19:17:23 -0700

    Change timing

--- 433Utils/RPi_utils/pwr.sh
+++ 433Utils/RPi_utils/pwr.sh
@@ -2,43 +2,44 @@
 
 device=$1
 operation=$2
+length=160
 
 CODESEND='/home/andy/bone/433Utils/RPi_utils/codesend'; 
 
 case "$device" in 
 "1") 
   if [ $operation = "on" ]; then 
-    $CODESEND 1381683 1 150
+    $CODESEND 1381683 1 $length 
     echo "Channel One On" 
   else 
-    $CODESEND 1381692 1 150
+    $CODESEND 1381692 1 $length 
     echo "Channel One Off" 
   fi
 ;;
 "2") 
   if [ $operation = "on" ]; then 
-    $CODESEND 1381827 1 150
+    $CODESEND 1381827 1 $length 
     echo "Channel Two On" 
   else 
-    $CODESEND 1381836 1 150
+    $CODESEND 1381836 1 $length
     echo "Channel Two Off" 
   fi
 ;;
 "3") 
   if [ $operation = "on" ]; then 
-    $CODESEND 1382147 1 150
+    $CODESEND 1382147 1 $length
     echo "Channel Three On" 
   else 
-    $CODESEND 1382156 1 150
+    $CODESEND 1382156 1 $length
     echo "Channel Three Off" 
   fi
 ;;
 "4") 
   if [ $operation = "on" ]; then 
-    $CODESEND 1383683 1 150
+    $CODESEND 1383683 1 $length 
     echo "Channel Four On" 
   else 
-    $CODESEND 1383692 1 150
+    $CODESEND 1383692 1 $length 
     echo "Channel Four Off" 
   fi
 ;;
