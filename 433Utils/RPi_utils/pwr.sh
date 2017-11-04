c85ed05f5106e9a2d1038c328acf9c990cba01ab
Andy Hughes <andyh@emerye.com>
2017-11-04 15:44:39 -0700

    Add 314

--- 433Utils/RPi_utils/pwr.sh
+++ 433Utils/RPi_utils/pwr.sh
@@ -14,7 +14,7 @@ function xmit {
   $CODESEND $1 $protocol $length
 } 
 
-
+#Channel 317
 case "$device" in 
 "1") 
   if [ $operation = "on" ]; then 
@@ -63,6 +63,54 @@ case "$device" in
     echo "Channel 317 Socket All Off" 
   fi
 ;;
-esac    
 
+#Channel 314
+"6") 
+  if [ $operation = "on" ]; then 
+    #317
+    xmit 1381683  
+#    $CODESEND 1381683 1 $length 
+    echo "Channel 317 Socket One On" 
+  else 
+    xmit 1381692 
+    echo "Channel 317 Socket One Off" 
+  fi
+;;
+"7") 
+  if [ $operation = "on" ]; then 
+    xmit 1381827 
+    echo "Channel 317 Socket Two On" 
+  else 
+    xmit 1381836
+    echo "Channel 317 Socket Two Off" 
+  fi
+;;
+"8") 
+  if [ $operation = "on" ]; then 
+    xmit 1382147 
+    echo "Channel 317 Socket Three On" 
+  else 
+    xmit 1382156
+    echo "Channel 317 Socket Three Off" 
+  fi
+;;
+"9") 
+  if [ $operation = "on" ]; then 
+    xmit 1383683 
+    echo "Channel 317 Socket Four On" 
+  else 
+    xmit 1383692 
+    echo "Channel 317 Socket Four Off" 
+  fi
+;;
+"10") 
+  if [ $operation = "on" ]; then 
+    xmit 1389827 
+    echo "Channel 317 Socket All On" 
+  else 
+    xmit 1389836
+    echo "Channel 317 Socket All Off" 
+  fi
+;;
+esac    
 
