1e09cf0c350e27b879e8c29d9fec70d2618ff960
Andy Hughes <andyh@emerye.com>
2017-12-06 10:31:49 -0800

    pngcrp.sh works for rcp

--- icon/rcpi.sh
+++ icon/rcpi.sh
@@ -7,9 +7,8 @@ convert toolicon.png -resize 16x16 toolicon_16_32.ico
 convert toolicon.png -resize 32x32 toolicon_32_32.ico
 convert toolicon.png -resize 48x48 toolicon_48_32.ico
 convert toolicon.png -resize 256x256 toolicon_256_32.ico
-convert toolicon.png -resize 16x16 -colors 256 -depth 8 toolicon_16_8.ico
-convert toolicon.png -resize 32x32 -colors 256 -depth 8 toolicon_32_8.ico
-convert toolicon.png -resize 48x48 -colors 256 -depth 8 toolicon_48_8.ico
-convert toolicon_16_32.ico toolicon_32_32.ico toolicon_48_32.ico \
-	toolicon_256_32.ico -colors 256 toolicon_16_8.ico toolicon_32_8.ico \
-	toolicon_48_8.ico toolicon.ico
+#convert toolicon.png -resize 16x16 -colors 256 -depth 8 toolicon_16_8.ico
+#convert toolicon.png -resize 32x32 -colors 256 -depth 8 toolicon_32_8.ico
+#convert toolicon.png -resize 48x48 -colors 256 -depth 8 toolicon_48_8.ico
+   convert toolicon_16_32.ico toolicon_32_32.ico toolicon_48_32.ico \
+	toolicon_256_32.ico toolicon2.ico
