ddca28c390b3e020cc336fb805ca74c0ab951133
Andy Hughes <andyh@emerye.com>
2018-03-05 23:18:41 -0800

    Update

--- py/lm75a.py
+++ py/lm75a.py
@@ -12,53 +12,39 @@ from smbus import SMBus
 from smbus2 import SMBusWrapper
 
 debug = False
-
-tableValue = ( 0xFA, 0x32, 0x1, 0, 0x01FF, 0x01CE, 0x0192)
+#NXp datasheet
+table11bit = ( 0x3F8, 0x3F7, 0x3F1, 0x3E8, 0xC8, 0x001, 0x00, 0x7FF, 0x738, 0x649, 0x648 )
 
 def ctofBytes(msb, lsb):
-    '''Convert LM75A data '''
-    degC = msb
-    if (msb >> 7) == 1:
-        negValue = (0xFF << 8) + msb
-        val = cvarTypes.c_int16(negValue)
-        degC = val.value
-        
-    if (lsb & 0x80) == 0x80:
-        degC = degC + 0.5
-        
-    retValue = degC * 1.8 + 32
-    if debug:
-        print('DegC %.1f DegF %.1f' % (degC, retValue)) 
-        
-    return retValue
+  bitWidth = 11
+  i = msb << 3 | lsb >> 5
+  if (i < 1024):
+      degC = i * 0.125
+  else:
+      degC = (i - int((i << 1) & 2**bitWidth)) * 0.125
+  return (degC * 1.8 + 32)
+
+
 
 tgtAddress = 0x4C
 devobj = SMBus(1)
 devobj.write_byte(tgtAddress, 0)
 
-
 if debug:
-  for i in tableValue: 
-    print('I',i)
-    if (i % 2) == 1:
-       lsb = 0x80
-    else:
-       lsb = 0
-            
-    msb = i >> 1
-    if debug: print('MSB %2x LSB %2x' % (msb, lsb))
-    val = ctofBytes(msb, lsb)
-    print('ctoBytes Temperature is %.1f\n' % (val))
-
-   
-for i in range(9):
+  for i in table11bit:
+      msb = i >> 3 & 0xFF
+      lsb = ((i & 0xFF) << 5) & 0xFF
+      if debug: print('11 Bit MSB %2x LSB %2x' % (msb, lsb))
+      val = ctofBytes(msb, lsb)
+     
+for i in range(20):
     devobj.write_byte(tgtAddress, 0)
     with SMBusWrapper(1) as bus:
         block = bus.read_i2c_block_data(tgtAddress, 0, 2)
         data = bytes(block)
         msb = data[0]
-        lsb = data[1] & 0x80
-        if debug: print('MSB %2x LSB %2x' % (msb, lsb))
+        lsb = data[1] & 0xE0
+        if debug: print('Bytes read from hardware: MSB %2x LSB %2x' % (msb, data[1]))
         tempF = ctofBytes(msb,lsb)
-        print('Temperature is %.1f\n' % (tempF))
+        print('Temperature is %.0f\n' % (tempF))
         time.sleep(1)
