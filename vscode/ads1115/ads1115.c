3a4a7e08ce7013a32ff741080842342fae5ce96d
Andy Hughes <andyh@emerye.com>
2020-10-25 14:56:28 -0700

    Add character display

---
+++ vscode/ads1115/ads1115.c
@@ -0,0 +1,128 @@
+/*
+ ============================================================================
+ Name        : adc1115.c
+ Author      : 
+ Version     :
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/ioctl.h>
+#include <i2c/smbus.h>
+#include <linux/i2c-dev.h>
+#include "wiringPi.h"
+#include "wiringPiI2C.h"
+#include "ads1115.h"
+#include "lcdchar.h"
+
+int handle;
+int i2caddr = 0x27;
+int dispfd;
+
+/// Take ADC reading, scale to gain and return value.
+double readADS1115()
+{
+
+	int adcReading;
+	unsigned char buffer[10];
+	double voltage;
+
+	usleep(2000);
+	read(handle, buffer, 2);
+	adcReading = ((buffer[0] << 8) | buffer[1]);
+	//voltage = (double)adcReading * (double)0.000125;  //4.096
+	voltage = (double)adcReading * (double)0.0001875; //6.144V
+	//voltage = (double)adcReading * (double)0.000015625; //256mvV
+	usleep(1000);
+	return voltage;
+}
+
+/// Write complete ADC configuration.
+int configADS1115(uint16_t adcCfg)
+{
+	unsigned char buffer[4];
+	int status;
+
+	buffer[0] = ADS1015_REG_POINTER_CONFIG;
+	buffer[1] = adcCfg >> 8;
+	buffer[2] = adcCfg & 0x00FF;
+
+	//Write to conversion register. This is a block write.
+	usleep(10000);
+	status = write(handle, buffer, 3);
+	if (status == -1)
+	{
+		printf("Error writing block to configuration register.\n");
+		return status;
+	}
+	wiringPiI2CWrite(handle, ADS1015_REG_POINTER_CONVERT);
+	usleep(20000);
+	return 0;
+}
+
+int intI2CcharDisplay()
+{
+	int r;
+	const char *dev = "/dev/i2c-1";
+
+	dispfd = open(dev, O_RDWR);
+	if (dispfd < 0)
+	{
+		perror("Opening i2c device node.\n");
+		return -1;
+	}
+	r = ioctl(dispfd, I2C_SLAVE, i2caddr);
+	if (r < 0)
+		perror("Selecting i2c device.\n");
+	Setup4bit(dispfd);
+	DisplayClear(dispfd);
+}
+
+int main(int argc, char *args[])
+{
+	int i, status;
+	double vMeasure;
+	double vMeasure1;
+	char strBuffer[256];
+
+	handle = wiringPiI2CSetup(ADS1015_ADDRESS);
+	if (handle < 0)
+	{
+		printf("Error number %d opening device.\n", handle);
+		return -1;
+	}
+	intI2CcharDisplay();
+	status = configADS1115(ADS1015_REG_CONFIG_MUX_SINGLE_0 | ADS1015_REG_CONFIG_PGA_6_144V |
+						   ADS1115_REG_CONFIG_DR_16SPS);
+	if (status == -1)
+	{
+		printf("Error configuring device.\n");
+		return -1;
+	}
+
+	for (i = 0; i < 50; i++)
+	{
+		configADS1115(ADS1015_REG_CONFIG_MUX_SINGLE_0 | ADS1015_REG_CONFIG_PGA_6_144V |
+					  ADS1115_REG_CONFIG_DR_16SPS);
+		vMeasure = readADS1115();
+		sprintf(strBuffer, "Ch1: %.3f       ", vMeasure);
+		WriteString(dispfd, 1, 0, strBuffer);
+
+		configADS1115(ADS1015_REG_CONFIG_MUX_SINGLE_1 | ADS1015_REG_CONFIG_PGA_6_144V |
+					  ADS1115_REG_CONFIG_DR_16SPS);
+		vMeasure1 = readADS1115();
+		sprintf(strBuffer, "Ch2: %.3f       ", vMeasure1);
+		WriteString(dispfd, 2, 0, strBuffer);
+		usleep(1000);
+	}
+	close(handle);
+
+	return EXIT_SUCCESS;
+}
---
+++ vscode/ads1115/lcdchar.h
@@ -0,0 +1,51 @@
+#ifndef LCDCHAR_H_
+#define LCDCHAR_H_
+
+#define RS 1
+#define READWRITE 2
+#define ENABLE 4
+#define BACKLED 8
+
+#define BIT0 1
+#define BIT1 2
+#define BIT2 4
+#define BIT3 8
+#define BIT4 0x10 
+#define BIT5 0x20
+#define BIT6 0x40 
+#define BIT7 0x80 
+
+/**
+ * Write a byte to the display. 
+ *
+ * @param i2cfd open i2c driver reference
+ * @param bytetoWrite Byte to write
+ * @param cmd 0=Command 1=Data
+ */ 
+extern void WriteI2CByte(int i2cfd, unsigned char bytetoWrite, int cmd); 
+
+/**
+ * Set up 4 bit interface
+ *
+ * @param i2cfd File descriptor 
+ */
+extern void Setup4bit(int i2cfd); 
+
+/**
+ * Clear Display
+ *
+ * @param i2cfd File descriptor 
+ */
+extern void DisplayClear(int i2cfd); 
+
+/**
+ * Write a string to the display at starting position
+ *
+ * @param i2cfd File descriptor 
+ * @param row Starting row number
+ * @param ypos Starting y position
+ * @param message[] String to send. 
+ */
+extern void WriteString(int i2cfd, int row, int ypos, char message[]); 
+
+#endif /* LCDCHAR_H_ */
