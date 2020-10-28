972ae6796a417175127c22b3ce9fe93708ba3d4e
Andy Hughes <andyh@emerye.com>
2020-10-28 16:57:54 -0700

    Add watt hours

--- vscode/timer/timer.c
+++ vscode/timer/timer.c
@@ -4,16 +4,18 @@
 
 timer_t gTimerid;
 int count = 0;
+double wattHours = 0;
+
 
 void start_timer(void)
 {
 
 struct itimerspec value;
 
-value.it_value.tv_sec = 5;//waits for 5 seconds before sending timer signal
+value.it_value.tv_sec = 1;//waits for 5 seconds before sending timer signal
 value.it_value.tv_nsec = 0;
 
-value.it_interval.tv_sec = 5;//sends timer signal every 5 seconds
+value.it_interval.tv_sec = 1;//sends timer signal every 5 seconds
 value.it_interval.tv_nsec = 0;
 
 timer_create (CLOCK_REALTIME, NULL, &gTimerid);
@@ -37,12 +39,16 @@ timer_settime (gTimerid, 0, &value, NULL);
 
 }
 
+
 void timer_callback(int sig) {
 
+double power = 203.7;
 count += 1;
-printf("Catch timer: %d  Count: %d\n", sig, count);
-
+//printf("Catch timer: %d  Count: %d\n", sig, count);
+wattHours = wattHours + power/3600;   //Watt minute
+printf("Seconds: %d  WattHr: %f\n",count, wattHours);
 }
+
 
 int main(int ac, char **av)
 {
