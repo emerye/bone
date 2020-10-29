4b8971a465efab9e1cc0ed7fd3b61a24eb12365a
Andy Hughes <andyh@emerye.com>
2020-10-29 16:45:46 -0700

    Format

--- vscode/timer/timer.c
+++ vscode/timer/timer.c
@@ -6,53 +6,43 @@ timer_t gTimerid;
 int count = 0;
 double wattHours = 0;
 
-
 void start_timer(void)
 {
+    struct itimerspec value;
 
-struct itimerspec value;
-
-value.it_value.tv_sec = 1;//waits for 5 seconds before sending timer signal
-value.it_value.tv_nsec = 0;
-
-value.it_interval.tv_sec = 1;//sends timer signal every 5 seconds
-value.it_interval.tv_nsec = 0;
-
-timer_create (CLOCK_REALTIME, NULL, &gTimerid);
-
-timer_settime (gTimerid, 0, &value, NULL);
-
+    value.it_value.tv_sec = 1; //waits for 1 seconds before sending timer signal
+    value.it_value.tv_nsec = 0;
+    value.it_interval.tv_sec = 1; //sends timer signal every 1 seconds
+    value.it_interval.tv_nsec = 0;
+    timer_create(CLOCK_REALTIME, NULL, &gTimerid);
+    timer_settime(gTimerid, 0, &value, NULL);
 }
 
 void stop_timer(void)
 {
+    struct itimerspec value;
 
-struct itimerspec value;
-
-value.it_value.tv_sec = 0;
-value.it_value.tv_nsec = 0;
-
-value.it_interval.tv_sec = 0;
-value.it_interval.tv_nsec = 0;
-
-timer_settime (gTimerid, 0, &value, NULL);
-
+    value.it_value.tv_sec = 0;
+    value.it_value.tv_nsec = 0;
+    value.it_interval.tv_sec = 0;
+    value.it_interval.tv_nsec = 0;
+    timer_settime(gTimerid, 0, &value, NULL);
 }
 
+void timer_callback(int sig)
+{
 
-void timer_callback(int sig) {
-
-double power = 203.7;
-count += 1;
-//printf("Catch timer: %d  Count: %d\n", sig, count);
-wattHours = wattHours + power/3600;   //Watt minute
-printf("Seconds: %d  WattHr: %f\n",count, wattHours);
+    double power = 203.7;
+    count += 1;
+    //printf("Catch timer: %d  Count: %d\n", sig, count);
+    wattHours = wattHours + power / 3600; //Watt minute
+    printf("Seconds: %d  WattHr: %f\n", count, wattHours);
 }
-
 
 int main(int ac, char **av)
 {
-(void) signal(SIGALRM, timer_callback);
-start_timer();
-while(1);
+    (void)signal(SIGALRM, timer_callback);
+    start_timer();
+    while (1)
+        ;
 }
