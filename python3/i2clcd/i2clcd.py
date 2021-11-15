35dc36f217049934c5c2c1ba5ae490ce1a9e5637
Andy Hughes <andyh@emerye.com>
2021-11-15 08:10:58 -0800

    Update project.

--- python3/i2clcd/i2clcd.py
+++ python3/i2clcd/i2clcd.py
@@ -1,21 +1,18 @@
 from smbus2 import SMBus
 from time import sleep
-import digitalio
 
-
-address = 0x27
-backlight = 1
+BACKLIGHT = 1
 ENABLE = 4
-
-LCD_CLEAR_DISPLAY = 1
-LCD_RETURNHOME = 2
-LCD_CURSOR_DISPLAY = 0x10
-LCD_DISPLAY_OFF = 0x08
-DISPLAY_SHIFT = 0x04
-LCD_FUNCTION_SET = 0x20
-
+address = 0x27
 
 def initLCD():
+    LCD_CLEAR_DISPLAY = 1
+    LCD_RETURNHOME = 2
+    LCD_CURSOR_DISPLAY = 0x10
+    LCD_DISPLAY_OFF = 0x08
+    DISPLAY_SHIFT = 0x04
+    LCD_FUNCTION_SET = 0x20
+
     sleep(0.015)
     writeLCDNibble(0x30,0)
     sleep(0.010)
@@ -40,26 +37,25 @@ def initLCD():
 
 # Write a byte of data to LCD display
 def writeLCDByte(bytetoWrite, commandType):
+    bus = SMBus(1)
 
-    highByte = (bytetoWrite & 0x00F0) + ENABLE + (backlight * 8) + commandType
+    highByte = (bytetoWrite & 0x00F0) + ENABLE + (BACKLIGHT * 8) + commandType
     bus.write_byte(address, highByte)
     highByte = highByte & ~ENABLE
     bus.write_byte(address, highByte)
-    bus.write_byte(address, highByte | ENABLE)
 
-    lowByte = ((bytetoWrite << 4) & 0x00F0) +ENABLE + (backlight * 8) + commandType
+    lowByte = ((bytetoWrite << 4) & 0x00F0) +ENABLE + (BACKLIGHT * 8) + commandType
     bus.write_byte(address, lowByte)
     lowByte = lowByte & ~ENABLE
     bus.write_byte(address, lowByte)
-    bus.write_byte(address, lowByte | ENABLE)
-
 
 def writeLCDNibble(bytetoWrite, commandType):
-    highByte = (bytetoWrite & 0x00F0) + ENABLE + (backlight * 8) + commandType
+    bus = SMBus(1)
+
+    highByte = (bytetoWrite & 0x00F0) + ENABLE + (BACKLIGHT * 8) + commandType
     bus.write_byte(address, highByte)
     highByte = highByte & ~ENABLE
     bus.write_byte(address, highByte)
-    bus.write_byte(address, highByte | ENABLE)
 
     # row 0 or 1
 def WriteString(row, pos, displayString):
@@ -69,26 +65,13 @@ def WriteString(row, pos, displayString):
     for x in displayString:
         writeLCDByte(ord(x),1)
 
-
-
 bus = SMBus(1)
 initLCD()
-writeLCDByte(0x80,0)    # Set address
-sleep(0.05)
-for x in range(0,2):
-    writeLCDByte(ord('n'), 1)
 
-writeLCDByte(0xC0,0)    # Set address
-sleep(0.05)
-for x in range(0,2):
-    writeLCDByte(ord('M'), 1)
-
-WriteString(0, 20, "Hello there Sunday")
+WriteString(0, 20, "Hello There Sunday")
 WriteString(1, 20, "Hello There")
-WriteString(0, 0, "Hello There")
-WriteString(1, 0, "Hello There")
-sleep(4)
-backlight = 0
-WriteString(1,60,"e")
+#WriteString(0, 0, "Hello There")
+#WriteString(1, 0, "Hello There")
+
 bus.close()
 
