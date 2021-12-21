3f935b3e989d3aabc3909ae057b778a6f292d90f
Andy Hughes <andyh@emerye.com>
2021-12-20 18:43:48 -0800

    1.0 volt ADC

--- python3/upesp32/ctest.py
+++ python3/upesp32/ctest.py
@@ -1,38 +1,44 @@
 from machine import ADC, Pin
+import machine
 import lcd
 import time
 
-adc9bitslope = 538.94
-adc9bitslope = 36.748
-
-#9bit ADC slope is 538.94 offset is 36.748 
-# ADCCnt = 538.94V -36.748
-# V = i(36.748 + ADCCnt)/538.94
 def adcReading(slope, offset, adcCount):
     return ((offset + adcCount) / slope)
 
 def adc12bitRead(samples):
     accum = 0
-    for i in range(1,samples):
+    for i in range(0,samples):
         adcRead = adc.read()
         accum = accum + adcRead
+        time.sleep(0.005)
     adcCnt = accum/samples
-    return(adcReading(4251.8892, 274.8816, adcCnt))
+    return(adcReading(4287.71384, 275.16018, adcCnt))
          
 
 lcdobj = lcd.CharLCD(0x27)
+inputPin = Pin(4, Pin.IN, Pin.PULL_UP)
 
 adc = ADC(Pin(32))          # create ADC object on ADC pin
 adc.atten(ADC.ATTN_0DB)    # set 0dB input attenuation (voltage range roughly 0.0v - 1.0v)
 adc.width(ADC.WIDTH_12BIT)   # set 9 bit return values (returned range 0-511)
 time.sleep(0.5)
 allCounts = 0
-for i in range(0, 99):
+data = []
+for i in range(0, 100):
     adcValue = adc.read()         
+    data.append(adcValue)
     allCounts = allCounts + adcValue 
+    time.sleep(0.005)
 
 buf = "Ave:%d L:%d" % (allCounts/100,adcValue)
-lcdobj.WriteString(1,0, buf)
-adcValue = adc12bitRead(100)
-reading = '%.4fV Sam:%d' % (adcValue, 100)
-lcdobj.WriteString(0,0,reading)
+lcdobj.WriteString(0, 20, buf)
+buf = "Max:%d Min:%d" % (max(data), min(data))
+lcdobj.WriteString(1, 20, buf)
+
+cnt = 0
+while inputPin.value() == 1: 
+    adcValue = adc12bitRead(100)
+    reading = '%.4fV' % adcValue
+    lcdobj.WriteString(0,0,reading)
+    cnt = cnt + 1
