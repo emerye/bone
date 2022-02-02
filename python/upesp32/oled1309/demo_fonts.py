57822209172f53b85b37dcd72937464dbfe59f34
Andy Hughes <andyh@emerye.com>
2022-02-01 17:14:43 -0800

    Remove some fonts

--- python/upesp32/oled1309/demo_fonts.py
+++ python/upesp32/oled1309/demo_fonts.py
@@ -11,27 +11,34 @@ def test():
     display = Display(spi, dc=Pin(4), cs=Pin(15), rst=Pin(5))
 
     print("Loading fonts.  Please wait.")
-    bally = XglcdFont('fonts/Bally7x9.c', 7, 9)
-    rototron = XglcdFont('fonts/Robotron13x21.c', 13, 21)
+  #  bally = XglcdFont('fonts/Bally7x9.c', 7, 9)
+   # rototron = XglcdFont('fonts/Robotron13x21.c', 13, 21)
     #unispace = XglcdFont('fonts/Unispace12x24.c', 12, 24)
     #wendy = XglcdFont('fonts/Wendy7x8.c', 7, 8)
-    perfect = XglcdFont('fonts/PerfectPixel_23x32.c', 23, 32)
+#    perfect = XglcdFont('fonts/PerfectPixel_23x32.c', 23, 32)
+    arial = XglcdFont('fonts/arial33x47per.c', 33, 47 ,start_letter=46,letter_count=12)
 
     print("Drawing fonts.")
 
     #text_height = bally.height
     #display.draw_text(display.width, display.height // 2 - text_height // 2, "Bally", bally, rotate=180)
 
-    text_width = rototron.measure_text("ROTOTRON")
-    display.draw_text(display.width // 2 - text_width // 2, 0, "ROTOTRON", rototron)
+   # text_width = rototron.measure_text("ROTOTRON")
+   # display.draw_text(display.width // 2 - text_width // 2, 0, "ROTOTRON", rototron)
 
     #text_width = unispace.measure_text("Unispace")
     #text_height = unispace.height
     #display.draw_text(display.width // 2 - text_width // 2, display.height - text_height, "Unispace", unispace, invert=True)
 
-    text_width = perfect.measure_text("1234")
-    text_height = perfect.height
-    display.draw_text(display.width // 2 - text_width // 2, display.height - text_height, "1234", perfect, invert=False)
+  #  text_width = perfect.measure_text("1234")
+  #  text_height = perfect.height
+  #  display.draw_text(display.width // 2 - text_width // 2, display.height - text_height, "1234", perfect, invert=False)
+
+    
+    text_width = arial.measure_text("0.9")
+    text_height = arial.height
+    display.draw_text(display.width // 2 - text_width // 2, display.height - text_height, "0.9", arial, invert=False)
+
         
     display.present()
 
