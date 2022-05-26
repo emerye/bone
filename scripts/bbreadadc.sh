f8597a2a7b66ab2c21a4b0f4e5251d3e3f8933a1
Andy Hughes <andyh@emerye.com>
2022-05-26 08:41:13 -0700

    Remove files

--- scripts/bbreadadc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while [ 1 ] 
-do
-cat /sys/bus/iio/devices/iio:device0/in_voltage1_raw
-sleep 1
-done
--- scripts/covid.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#git@github.com:nytimes/covid-19-data.git
-
-grep Alameda /home/andy/covid-19-data/us-counties.csv > alameda.csv 
