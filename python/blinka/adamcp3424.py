fedbdb05696d5140184687d3f24da25357e49728
Andy Hughes <andyh@emerye.com>
2025-07-14 12:24:14 -0700

    Add readme

--- python/blinka/adamcp3424.py
+++ python/blinka/adamcp3424.py
@@ -9,18 +9,19 @@ import binascii
 from rpi_lcd import LCD
 from smbus2 import SMBus
 from time import sleep
+from adafruit_bus_device.i2c_device import I2CDevice
 
 #set to zero to remove LCD character display
 CHARDISP = 0
 
-def printlist():
-    print(f"{count} ADC value: {adc_channel.value}")
+def printlist(ADC):
+    print(f"ADC value: {adc_channel.value}")
     if adc.continuous_mode:
         mode = "continuous"
     else:
         mode = "one-shot"
     print(f"Mode: {mode}")
-    print(f"ADC Voltage: {volts:.4f}")
+    #print(f"ADC Voltage: {volts:.4f}")
     print(f"Current gain: {adc.gain}X")
     print(f"Current resolution: {adc.resolution}-bit")
     
@@ -45,8 +46,21 @@ def radc():
             print("File not found!", file=sys.stderr)
             pass
 
-    adc = ADC.MCP3421(i2c, gain=8, resolution=14, continuous_mode=True)
+
+    adc = ADC.MCP3421(i2c, gain=1, resolution=14, continuous_mode=True, channel=2)
     adc_channel = AnalogIn(adc)
+    i2c_device = I2CDevice(i2c, 0x68)
+    with i2c_device as bus_device:
+        result = bytearray(3)
+        bus_device.readinto(result)
+        print(result.hex())
+        
+        config_byte = bytearray(b'\xb7')
+        bus_device.write(bytearray(config_byte))
+    
+        result = bytearray(6)
+        bus_device.readinto(result)
+        print(result.hex())
     
     
 # gain, resolution and mode can also be set after instantiation:
@@ -61,22 +75,42 @@ def radc():
 
 # set continuous read mode True or False for one-shot
 # defaults to True
-# adc.continuous_mode = True
+#    adc.continuous_mode = False
+#    adc.channel = 2
+    adc.gain = 1
+#    adc.continuous_mode = True
+  
+
+#Current reading
     count = 0
     while True:
+        #Current reading
         count = count + 1
+        with i2c_device as bus_device:
+            config_byte = bytearray(b'\xb4')  #gain 1
+            bus_device.write(bytearray(config_byte))
+        
+        sleep(0.05)
         volts = adc_channel.value * 0.000250
         current = volts * 6.66 
-        
         vString = f"{count} {volts:.4f} {current:.2f}"
-        
-        
         if CHARDISP != 0:
             lcdobj.text(vString, 3)
             
-       
-        print(f"{count} ADC Voltage: {volts:.4f} Current: {current:.2f}")
+        print(f"{count} ADC: {adc_channel.value} ADC Voltage: {volts:.4f} Current: {current:.2f}")
         
+        #50 Volt reading
+        with i2c_device as bus_device:
+            result = bytearray(6)
+            bus_device.readinto(result)
+            print(result.hex())
+            
+        with i2c_device as bus_device:
+            config_byte = bytearray(b'\xC4')  #gain 1
+            bus_device.write(bytearray(config_byte))
+        sleep(0.05)
+        volts = adc_channel.value * 0.000250
+        print(f"Channel 3: {adc_channel.value} ADC Voltage: {volts:.4f}")
         
         #printlist()
         try:
--- python/mcp3424iio.py
+++ python/mcp3424iio.py
@@ -1,4 +1,7 @@
-import sys
+#!/usr/bin/env python3
+
+
+import os
 
 #from os.path import isdir, isfile, join, realpath, normpath
 #import subprocess
@@ -7,25 +10,34 @@ from time import sleep
 # Add mcp3422 to /etc/modules
 ADCSETUP ="/sys/bus/i2c/devices/i2c-1/new_device"
 DEVDIR = "/sys/bus/iio/devices/iio:device0/"
+SCALE = '0.00025' # X4
+SCALE = '0.0005' # X2
+SCALE = '0.001'  # X1
 
 def init():
-    open(ADCSETUP,'w').write(str("mcp3424 0x68"))
+    path = os.path.realpath(ADCSETUP)
+    if not os.path.isfile(path):
+        print(f"Error directory {path} does not exist. Exiting!")
+        exit()
+        
+ #   open(ADCSETUP,'w').write(str("mcp3424 0x68"))
+ #   p = DEVDIR + 'in_voltage2_scale'
+ #   f = open(p,'w+').write(SCALE)
     
-    p = DEVDIR + 'in_voltage2_scale'
-    f = open(p,'w+').write('0.001')
-    #f.flush()
-    sleep(0.1)
-    
-init()    
+#main
+#ls
+#init()
+ 
+#Setup for mcp3422  Channel 1
 
-p = (DEVDIR + 'in_voltage2_raw')
+p = (DEVDIR + '/in_voltage0_raw')
 
 for x in range(10):
-    sleep(1.1)
+    sleep(0.5)
     f = open(p,'r')
     volt = f.readline().strip()
 
-    print(volt)
+    print(int(volt))
 
 
     
